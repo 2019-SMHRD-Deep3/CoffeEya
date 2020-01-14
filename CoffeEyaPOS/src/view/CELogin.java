b4b66ae7125fcade204f2f26fed02763b9fbdb27
SM015 <SM015@DESKTOP-N7C3MC9>
2020-01-14 20:52:28 +0900

    login/main/bg commit!!

--- CoffeEyaPOS/src/view/CELogin.java
+++ CoffeEyaPOS/src/view/CELogin.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,12 +17,15 @@ import model.Member;
 import view.CEMain;
 
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import java.awt.Font;
 
 public class CELogin {
 	private MemberManagementService service = new MemberManagementService();
@@ -32,6 +36,7 @@ public class CELogin {
 	private JPanel panel_1;
 	private JButton btnNewButton;
 	private JButton btnNewButton_1;
+	private JPanel panel;
 
 	/**
 	 * Launch the application.
@@ -64,37 +69,52 @@ public class CELogin {
 		frame.setBounds(100, 100, 1200, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(300, 250, 600, 300);
+
+		String imgPath = this.getClass().getResource(".").getPath() + "..//..//CoffeEyaIMG//loginCoffee.jpg";
+		ImageIcon icon = new ImageIcon(imgPath);
+
+		panel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				g.drawImage(icon.getImage(), 0, 0, panel.getWidth(), panel.getHeight(), null);
+				setOpaque(false);
+				super.paintComponent(g);
+			}
+		};
+
+		panel.setBounds(0, 0, 1200, 800);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("\uC544\uC774\uB514");
-		lblNewLabel.setBounds(10, 10, 190, 90);
+		lblNewLabel.setBounds(229, 179, 190, 90);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		panel.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("\uBE44\uBC00\uBC88\uD638");
-		lblNewLabel_1.setBounds(10, 110, 190, 90);
+		lblNewLabel_1.setBounds(229, 291, 190, 90);
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		panel.add(lblNewLabel_1);
-		
+
 		textField = new JTextField();
-		textField.setBounds(210, 10, 380, 90);
+		textField.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		textField.setBackground(SystemColor.inactiveCaptionBorder);
+		textField.setBounds(515, 180, 380, 90);
 		panel.add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
-		textField_1.setBounds(210, 110, 380, 90);
+		textField_1.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		textField_1.setBackground(SystemColor.inactiveCaptionBorder);
+		textField_1.setBounds(515, 292, 380, 90);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		panel_1 = new JPanel();
-		panel_1.setBounds(210, 220, 380, 60);
+		panel_1.setBounds(413, 465, 380, 60);
 		panel.add(panel_1);
-		panel_1.setLayout(new GridLayout(1, 0, 20, 0));
-		
+		panel_1.setLayout(new GridLayout(1, 0, 5, 0));
+
 		btnNewButton = new JButton("\uB85C\uADF8\uC778");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -118,7 +138,7 @@ public class CELogin {
 			}
 		});
 		panel_1.add(btnNewButton);
-		
+
 		btnNewButton_1 = new JButton("\uC885\uB8CC");
 		btnNewButton_1.addMouseListener(new MouseAdapter() {
 			@Override
@@ -127,5 +147,8 @@ public class CELogin {
 			}
 		});
 		panel_1.add(btnNewButton_1);
+
 	}
+
+
 }
