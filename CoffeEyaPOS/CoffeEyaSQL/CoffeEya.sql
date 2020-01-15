f68db5d2910047f0098ab754787dfbae7ecf8559
SM111 <SM111@DESKTOP-QM0P8HJ>
2020-01-15 10:18:56 +0900

    sql더미파일

--- CoffeEyaPOS/CoffeEyaSQL/CoffeEya.sql
+++ CoffeEyaPOS/CoffeEyaSQL/CoffeEya.sql
@@ -1,5 +1,11 @@
+drop table member cascade CONSTRAINTS;
+drop table product cascade CONSTRAINTS;
+drop table ordering cascade CONSTRAINTS;
+drop table detail;
+
+
 --------------------------------------------------------
---  ÆÄÀÏÀÌ »ý¼ºµÊ - È­¿äÀÏ-1¿ù-14-2020   
+--  ÆÄÀÏÀÌ »ý¼ºµÊ - ¼ö¿äÀÏ-1¿ù-15-2020   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Table MEMBER
@@ -11,25 +17,32 @@
 	"MEM_NAME" VARCHAR2(20 BYTE), 
 	"MEM_PERM" VARCHAR2(20 BYTE)
    ) ;
-   
+REM INSERTING into MEMBER
 SET DEFINE OFF;
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('°ûÃ¢Çõ10','1111','°ûÃ¢Çõ10','940511');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('°ûÃ¢Çõ','1111','°ûÃ¢Çõ','940511');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('°ûªFÇõ','1111','°ûÃ¢Çõ2','4949449');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('ehdrud1129','1212','³ëµ¿°æ','921129');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('kjh','1111','±èÁØÇü','19910614');
+Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('°ûÃ¢Çõ','1111','°ûÃ¢Çõ','manager');
+Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('³ëµ¿°æ','1111','³ëµ¿°æ','manager');
+Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('±èÁØÇü','1111','±èÁØÇü','manager');
+Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('±èÈ¿Áø','1111','±èÁØÇü','manager');
+Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('1111','1111','1111','employee');
+--------------------------------------------------------
+--  DDL for Index MEMBER_PK
+--------------------------------------------------------
 
-  CREATE UNIQUE INDEX "MEMBER_PK" ON "MEMBER1" ("MEM_ID") 
+  CREATE UNIQUE INDEX "MEMBER_PK" ON "MEMBER" ("MEM_ID") 
   ;
+--------------------------------------------------------
+--  Constraints for Table MEMBER
+--------------------------------------------------------
 
-  ALTER TABLE "MEMBER" ADD CONSTRAINT "MEMBER_PK" PRIMARY KEY ("MEM_ID") ENABLE;
   ALTER TABLE "MEMBER" MODIFY ("MEM_ID" NOT NULL ENABLE);
+  ALTER TABLE "MEMBER" ADD CONSTRAINT "MEMBER_PK" PRIMARY KEY ("MEM_ID") ENABLE;
+  
   
   
   
   
   --------------------------------------------------------
---  ÆÄÀÏÀÌ »ý¼ºµÊ - È­¿äÀÏ-1¿ù-14-2020   
+--  ÆÄÀÏÀÌ »ý¼ºµÊ - ¼ö¿äÀÏ-1¿ù-15-2020   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Table PRODUCT
@@ -40,19 +53,29 @@ Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('kjh','1111','±èÁØÇ
 	"PRO_NAME" VARCHAR2(20 BYTE), 
 	"PRO_PRICE" NUMBER(10,0)
    ) ;
-
+REM INSERTING into PRODUCT
 SET DEFINE OFF;
+Insert into PRODUCT (PRO_NUM,PRO_NAME,PRO_PRICE) values (1,'¾Æ¸Þ¸®Ä«³ë',3000);
+Insert into PRODUCT (PRO_NUM,PRO_NAME,PRO_PRICE) values (2,'¿¡½ºÇÁ·¹¼Ò',2500);
+Insert into PRODUCT (PRO_NUM,PRO_NAME,PRO_PRICE) values (3,'Ä«Æä¶ó¶¼',3500);
+Insert into PRODUCT (PRO_NUM,PRO_NAME,PRO_PRICE) values (4,'Ä«Æä¸ðÄ«',4500);
+Insert into PRODUCT (PRO_NUM,PRO_NAME,PRO_PRICE) values (5,'È­ÀÌÆ®¸ðÄ«',5500);
+--------------------------------------------------------
+--  DDL for Index MENU_PK
+--------------------------------------------------------
 
   CREATE UNIQUE INDEX "MENU_PK" ON "PRODUCT" ("PRO_NUM") 
   ;
+--------------------------------------------------------
+--  Constraints for Table PRODUCT
+--------------------------------------------------------
 
-  ALTER TABLE "PRODUCT" ADD CONSTRAINT "MENU_PK" PRIMARY KEY ("PRO_NUM") ENABLE;
   ALTER TABLE "PRODUCT" MODIFY ("PRO_NUM" NOT NULL ENABLE);
-
+  ALTER TABLE "PRODUCT" ADD CONSTRAINT "MENU_PK" PRIMARY KEY ("PRO_NUM") ENABLE;
 
 
 --------------------------------------------------------
---  ÆÄÀÏÀÌ »ý¼ºµÊ - È­¿äÀÏ-1¿ù-14-2020   
+--  ÆÄÀÏÀÌ »ý¼ºµÊ - ¼ö¿äÀÏ-1¿ù-15-2020   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Table ORDERING
@@ -65,14 +88,27 @@ SET DEFINE OFF;
 	"OR_SUM" NUMBER(10,0), 
 	"MEM_ID" VARCHAR2(20 BYTE)
    ) ;
-
+REM INSERTING into ORDERING
 SET DEFINE OFF;
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (1,to_date('20/01/01','RR/MM/DD'),'Ä«µå',6000,'°ûÃ¢Çõ');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (2,to_date('20/01/01','RR/MM/DD'),'Ä«µå',10000,'°ûÃ¢Çõ');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (3,to_date('20/01/02','RR/MM/DD'),'Çö±Ý',9500,'³ëµ¿°æ');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (4,to_date('20/01/02','RR/MM/DD'),'Çö±Ý',8000,'³ëµ¿°æ');
+--------------------------------------------------------
+--  DDL for Index ORDERING_PK
+--------------------------------------------------------
 
   CREATE UNIQUE INDEX "ORDERING_PK" ON "ORDERING" ("OR_NUM") 
   ;
+--------------------------------------------------------
+--  Constraints for Table ORDERING
+--------------------------------------------------------
 
-  ALTER TABLE "ORDERING" ADD CONSTRAINT "ORDERING_PK" PRIMARY KEY ("OR_NUM") ENABLE;
   ALTER TABLE "ORDERING" MODIFY ("OR_NUM" NOT NULL ENABLE);
+  ALTER TABLE "ORDERING" ADD CONSTRAINT "ORDERING_PK" PRIMARY KEY ("OR_NUM") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table ORDERING
+--------------------------------------------------------
 
   ALTER TABLE "ORDERING" ADD CONSTRAINT "ORDERING_NUMBER_FK" FOREIGN KEY ("MEM_ID")
 	  REFERENCES "MEMBER" ("MEM_ID") ENABLE;
@@ -81,7 +117,7 @@ SET DEFINE OFF;
 
 
 --------------------------------------------------------
---  ÆÄÀÏÀÌ »ý¼ºµÊ - È­¿äÀÏ-1¿ù-14-2020   
+--  ÆÄÀÏÀÌ »ý¼ºµÊ - ¼ö¿äÀÏ-1¿ù-15-2020   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Table DETAIL
@@ -93,17 +129,36 @@ SET DEFINE OFF;
 	"PRO_NUM" NUMBER(10,0), 
 	"DE_AMOUNT" NUMBER(10,0)
    ) ;
-
+REM INSERTING into DETAIL
 SET DEFINE OFF;
+Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (1,1,1,2);
+Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (2,2,1,1);
+Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (3,2,2,1);
+Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (4,2,4,1);
+Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (5,3,2,2);
+Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (6,3,4,1);
+Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (7,4,3,1);
+Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (8,4,4,1);
+--------------------------------------------------------
+--  DDL for Index DETAIL_PK
+--------------------------------------------------------
 
   CREATE UNIQUE INDEX "DETAIL_PK" ON "DETAIL" ("DE_NUM") 
   ;
+--------------------------------------------------------
+--  Constraints for Table DETAIL
+--------------------------------------------------------
 
-  ALTER TABLE "DETAIL" ADD CONSTRAINT "DETAIL_PK" PRIMARY KEY ("DE_NUM") ENABLE;
   ALTER TABLE "DETAIL" MODIFY ("DE_NUM" NOT NULL ENABLE);
+  ALTER TABLE "DETAIL" ADD CONSTRAINT "DETAIL_PK" PRIMARY KEY ("DE_NUM") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table DETAIL
+--------------------------------------------------------
 
   ALTER TABLE "DETAIL" ADD CONSTRAINT "DETAIL_ORDERING_FK" FOREIGN KEY ("OR_NUM")
 	  REFERENCES "ORDERING" ("OR_NUM") ENABLE;
   ALTER TABLE "DETAIL" ADD CONSTRAINT "DETAIL_PRODUCT_FK" FOREIGN KEY ("PRO_NUM")
 	  REFERENCES "PRODUCT" ("PRO_NUM") ENABLE;
 
+  
+  
