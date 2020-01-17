a9bd0837f5428c015ecb144fdb67f960e8580566
SM111 <SM111@DESKTOP-QM0P8HJ>
2020-01-17 21:44:11 +0900

    commit

--- CoffeEyaPOS/CoffeEyaSQL/CoffeEya.sql
+++ CoffeEyaPOS/CoffeEyaSQL/CoffeEya.sql
@@ -1,8 +1,8 @@
 drop table member cascade CONSTRAINTS;
 drop table product cascade CONSTRAINTS;
 drop table ordering cascade CONSTRAINTS;
-drop table detail cascade CONSTRAINTS;
-drop table customer cascade CONSTRAINTS;
+drop table detail;
+drop table customer;
 
 
 --------------------------------------------------------
@@ -26,14 +26,8 @@ Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('곽창혁','1111','곽
 Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('노동경','1111','노동경','manager');
 Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('김준형','1111','김준형','manager');
 Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('김효진','1111','김준형','manager');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('1111','1111','오원일','manager');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('2222','2222','아르바이트생2','employee');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('3333','3333','아르바이트생3','employee');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('4444','4444','아르바이트생4','employee');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('5555','5555','아르바이트생5','employee');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('6666','6666','아르바이트생6','employee');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('7777','7777','아르바이트생7','employee');
-Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('오원일','1111','오원일','manager');
+Insert into MEMBER (MEM_ID,MEM_PW,MEM_NAME,MEM_PERM) values ('1111','1111','아르바이트생1','employee');
+
 --------------------------------------------------------
 --  DDL for Index MEMBER_PK
 --------------------------------------------------------
@@ -124,62 +118,17 @@ Insert into PRODUCT (PRO_NUM,PRO_NAME,PRO_PRICE) values (28,'조각케익',5000);
    ) ;
 REM INSERTING into ORDERING
 SET DEFINE OFF;
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (1,to_date('20/01/01','RR/MM/DD'),'카드',6000,'1111');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (2,to_date('20/01/01','RR/MM/DD'),'카드',10000,'1111');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (3,to_date('20/01/02','RR/MM/DD'),'현금',9500,'1111');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (4,to_date('20/01/02','RR/MM/DD'),'카드',18000,'1111');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (5,to_date('20/01/03','RR/MM/DD'),'현금',22000,'1111');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (6,to_date('20/01/03','RR/MM/DD'),'현금',22000,'5555');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (7,to_date('20/01/03','RR/MM/DD'),'카드',18000,'5555');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (8,to_date('20/01/04','RR/MM/DD'),'현금',4000,'5555');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (9,to_date('20/01/04','RR/MM/DD'),'카드',6000,'5555');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (10,to_date('20/01/04','RR/MM/DD'),'카드',10000,'5555');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (11,to_date('20/01/04','RR/MM/DD'),'현금',9500,'5555');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (12,to_date('20/01/04','RR/MM/DD'),'카드',18000,'6666');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (13,to_date('20/01/05','RR/MM/DD'),'현금',22000,'6666');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (14,to_date('20/01/05','RR/MM/DD'),'카드',22000,'6666');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (15,to_date('20/01/05','RR/MM/DD'),'현금',18000,'6666');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (16,to_date('20/01/06','RR/MM/DD'),'현금',4000,'6666');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (17,to_date('20/01/06','RR/MM/DD'),'카드',6000,'6666');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (18,to_date('20/01/06','RR/MM/DD'),'카드',10000,'6666');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (19,to_date('20/01/06','RR/MM/DD'),'현금',9500,'6666');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (20,to_date('20/01/07','RR/MM/DD'),'현금',18000,'6666');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (21,to_date('20/01/07','RR/MM/DD'),'현금',22000,'2222');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (22,to_date('20/01/07','RR/MM/DD'),'카드',22000,'2222');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (23,to_date('20/01/07','RR/MM/DD'),'카드',18000,'2222');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (24,to_date('20/01/07','RR/MM/DD'),'카드',4000,'2222');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (25,to_date('20/01/07','RR/MM/DD'),'카드',6000,'2222');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (26,to_date('20/01/08','RR/MM/DD'),'카드',10000,'2222');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (27,to_date('20/01/08','RR/MM/DD'),'현금',9500,'2222');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (28,to_date('20/01/08','RR/MM/DD'),'현금',18000,'2222');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (29,to_date('20/01/08','RR/MM/DD'),'카드',22000,'7777');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (30,to_date('20/01/08','RR/MM/DD'),'카드',22000,'7777');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (31,to_date('20/01/08','RR/MM/DD'),'카드',8000,'7777');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (32,to_date('20/01/08','RR/MM/DD'),'현금',4000,'7777');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (33,to_date('20/01/08','RR/MM/DD'),'카드',6000,'7777');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (34,to_date('20/01/09','RR/MM/DD'),'카드',10000,'7777');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (35,to_date('20/01/09','RR/MM/DD'),'현금',9500,'7777');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (36,to_date('20/01/09','RR/MM/DD'),'카드',18000,'3333');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (37,to_date('20/01/09','RR/MM/DD'),'카드',22000,'3333');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (38,to_date('20/01/09','RR/MM/DD'),'현금',22000,'3333');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (39,to_date('20/01/09','RR/MM/DD'),'카드',18000,'3333');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (40,to_date('20/01/09','RR/MM/DD'),'현금',4000,'3333');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (41,to_date('20/01/10','RR/MM/DD'),'카드',6000,'3333');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (42,to_date('20/01/10','RR/MM/DD'),'카드',10000,'3333');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (43,to_date('20/01/10','RR/MM/DD'),'현금',9500,'3333');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (44,to_date('20/01/10','RR/MM/DD'),'카드',18000,'3333');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (45,to_date('20/01/04','RR/MM/DD'),'카드',22000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (46,to_date('20/01/10','RR/MM/DD'),'현금',22000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (47,to_date('20/01/11','RR/MM/DD'),'현금',18000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (48,to_date('20/01/11','RR/MM/DD'),'현금',4000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (49,to_date('20/01/11','RR/MM/DD'),'카드',6000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (50,to_date('20/01/11','RR/MM/DD'),'카드',10000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (51,to_date('20/01/12','RR/MM/DD'),'카드',9500,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (52,to_date('20/01/12','RR/MM/DD'),'카드',18000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (53,to_date('20/01/12','RR/MM/DD'),'카드',22000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (54,to_date('20/01/12','RR/MM/DD'),'현금',22000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (55,to_date('20/01/12','RR/MM/DD'),'카드',18000,'4444');
-Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (56,to_date('20/01/12','RR/MM/DD'),'현금',4000,'4444');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (1,to_date('20/01/01','RR/MM/DD'),'카드',6000,'곽창혁');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (2,to_date('20/01/01','RR/MM/DD'),'카드',10000,'김효진');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (3,to_date('20/01/02','RR/MM/DD'),'현금',9500,'김준형');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (4,to_date('20/01/02','RR/MM/DD'),'카드',18000,'김준형');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (5,to_date('20/01/03','RR/MM/DD'),'현금',22000,'노동경');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (6,to_date('20/01/03','RR/MM/DD'),'현금',22000,'곽창혁');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (7,to_date('20/01/03','RR/MM/DD'),'카드',18000,'노동경');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (8,to_date('20/01/04','RR/MM/DD'),'현금',4000,'김효진');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (9,to_date('20/01/04','RR/MM/DD'),'카드',6000,'김효진');
+Insert into ORDERING (OR_NUM,OR_DATE,OR_PAY,OR_SUM,MEM_ID) values (10,to_date('20/01/04','RR/MM/DD'),'카드',10000,'곽창혁');
+
 ------------------------------------------------------
 --  DDL for Index ORDERING_PK
 --------------------------------------------------------
@@ -247,68 +196,6 @@ Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (27,9,20,1);
 Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (28,9,22,1);
 Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (29,10,17,2);
 Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (30,10,13,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (31,11,1,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (32,11,1,8);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (33,12,17,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (34,13,19,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (35,14,2,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (36,14,2,9);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (37,14,8,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (38,14,9,4);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (39,14,9,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (40,15,3,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (41,16,3,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (42,17,1,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (43,18,1,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (44,18,1,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (45,19,1,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (46,20,1,4);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (47,21,8,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (48,22,10,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (49,23,6,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (50,23,6,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (51,24,7,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (52,24,8,8);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (53,25,8,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (54,26,10,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (55,27,11,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (56,28,1,9);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (57,29,11,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (58,30,20,4);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (59,31,11,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (60,32,1,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (53,33,2,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (54,34,2,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (55,35,14,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (56,36,12,9);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (57,37,1,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (58,38,7,4);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (59,39,2,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (60,40,2,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (61,41,8,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (62,42,21,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (63,43,21,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (64,44,10,9);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (65,45,10,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (66,46,6,4);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (67,47,3,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (68,48,1,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (69,49,2,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (70,50,2,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (71,51,1,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (72,51,1,9);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (73,52,1,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (74,53,6,4);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (75,53,3,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (76,53,1,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (77,53,2,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (78,54,2,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (79,55,1,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (80,56,1,9);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (81,56,1,2);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (82,56,6,4);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (83,56,3,1);
-Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (84,56,1,1);
 --------------------------------------------------------
 --  DDL for Index DETAIL_PK
 --------------------------------------------------------
@@ -338,23 +225,23 @@ Insert into DETAIL (DE_NUM,OR_NUM,PRO_NUM,DE_AMOUNT) values (84,56,1,1);
 --------------------------------------------------------
 
   CREATE TABLE "CUSTOMER" 
-   (	"CUS_NUMBER" VARCHAR2(40 BYTE), 
-	"CUS_NAME" VARCHAR2(40 BYTE), 
-	"CUS_PHONE" VARCHAR2(40 BYTE), 
-	"CUS_SEX" VARCHAR2(40 BYTE), 
-	"CUS_BIRTHDAY" DATE, 
-	"CUS_POINT" NUMBER(20,0)
+   (   "CUS_NUMBER" VARCHAR2(40 BYTE), 
+   "CUS_NAME" VARCHAR2(40 BYTE), 
+   "CUS_PHONE" VARCHAR2(40 BYTE), 
+   "CUS_SEX" VARCHAR2(40 BYTE), 
+   "CUS_BIRTHDAY" DATE, 
+   "CUS_POINT" NUMBER(20,0)
    ) ;
 REM INSERTING into CUSTOMER
 SET DEFINE OFF;
-Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values ('7001','김혜리','010-8447-4564','FEMALE',to_date('98/11/04','RR/MM/DD'),1320);
-Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values ('7002','고성원','010-4644','MALE',to_date('96/03/02','RR/MM/DD'),260);
-Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values ('7003','이재욱','010-3628','MALE',to_date('94/08/12','RR/MM/DD'),873);
-Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values ('7004','윤지혜','010-8487-4564','FEMALE',to_date('90/02/09','RR/MM/DD'),1358);
-Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values ('7005','강혜성','010-4644-4852','FEMALE',to_date('98/08/17','RR/MM/DD'),1351);
-Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values ('7006','이재원','010-3628-4491','FEMALE',to_date('99/11/06','RR/MM/DD'),5142);
-Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values ('7007','김인우','010-5496-8125','MALE',to_date('00/07/08','RR/MM/DD'),3213);
-Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values ('7008','유지윤','010-8486-1138','MALE',to_date('00/02/12','RR/MM/DD'),153);
+Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values (7001,'김혜리','010-8447-4564','FEMALE',to_date('98/11/04','RR/MM/DD'),1320);
+Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values (7002,'고성원','010-4644','MALE',to_date('96/03/02','RR/MM/DD'),260);
+Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values (7003,'이재욱','010-3628','MALE',to_date('94/08/12','RR/MM/DD'),873);
+Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values (7004,'윤지혜','010-8487-4564','FEMALE',to_date('90/02/09','RR/MM/DD'),1358);
+Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values (7005,'강혜성','010-4644-4852','FEMALE',to_date('98/08/17','RR/MM/DD'),1351);
+Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values (7006,'이재원','010-3628-4491','FEMALE',to_date('99/11/06','RR/MM/DD'),5142);
+Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values (7007,'김인우','010-5496-8125','MALE',to_date('00/07/08','RR/MM/DD'),3213);
+Insert into CUSTOMER (CUS_NUMBER,CUS_NAME,CUS_PHONE,CUS_SEX,CUS_BIRTHDAY,CUS_POINT) values (7008,'유지윤','010-8486-1138','MALE',to_date('00/02/12','RR/MM/DD'),153);
 --------------------------------------------------------
 --  DDL for Index CUSTOMER_PK
 --------------------------------------------------------
