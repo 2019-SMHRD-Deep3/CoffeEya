f8891d8c0107a1d3c58ad1170151afd77e3c6302
SM111 <SM111@DESKTOP-QM0P8HJ>
2020-01-18 18:03:00 +0900

    판매 DB연동

---
+++ CoffeEyaPOS/src/controller/OrderingManagementService.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.Ordering;
+import model.OrderingDAO;
+import model.Product;
+
+public class OrderingManagementService {
+
+	private OrderingDAO dao = new OrderingDAO();
+
+	public boolean OrderingJoin(Ordering o) {
+		int rows = dao.insert(o);
+		if (rows == 0)
+			return false;
+		else
+			return true;
+	}
+
+	public int OrderingCount() {
+
+		return dao.orderingCount();
+	}
+
+}
