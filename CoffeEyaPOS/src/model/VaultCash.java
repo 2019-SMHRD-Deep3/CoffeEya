c238abf38dc90810c2805bfc70d2ca8648e9c64f
SM016 <SM016@DESKTOP-04HC7FI>
2020-01-19 20:05:21 +0900

    수정 미완성

---
+++ CoffeEyaPOS/src/model/VaultCash.java
@@ -0,0 +1,95 @@
+package model;
+
+public class VaultCash {
+	private int M1;
+	private int M2;
+	private int M3;
+	private int M4;
+	private int M5;
+	private int M6;
+	private int M7;
+	private int M8;
+	
+	public VaultCash() {
+		
+	}
+	
+	public VaultCash(int M1, int M2, int M3, int M4, int M5, int M6, int M7, int M8) {
+		this.M1 = M1;
+		this.M2 = M2;
+		this.M3 = M3;
+		this.M4 = M4;
+		this.M5 = M5;
+		this.M6 = M6;
+		this.M7 = M7;
+		this.M8 = M8;
+	}
+	
+
+	public int getM1() {
+		return M1;
+	}
+
+	public int getM2() {
+		return M2;
+	}
+
+	public int getM3() {
+		return M3;
+	}
+
+	public int getM4() {
+		return M4;
+	}
+
+	public int getM5() {
+		return M5;
+	}
+
+	public int getM6() {
+		return M6;
+	}
+
+	public int getM7() {
+		return M7;
+	}
+
+	public int getM8() {
+		return M8;
+	}
+
+	public void setM1(int M1) {
+		this.M1 = M1;
+	}
+
+	public void setM2(int M2) {
+		this.M2 = M2;
+	}
+
+	public void setM3(int M3) {
+		this.M3 = M3;
+	}
+
+	public void setM4(int M4) {
+		this.M4 = M4;
+	}
+
+	public void setM5(int M5) {
+		this.M5 = M5;
+	}
+
+	public void setM6(int M6) {
+		this.M6 = M6;
+	}
+
+	public void setM7(int M7) {
+		this.M7 = M7;
+	}
+
+	public void setM8(int M8) {
+		this.M8 = M8;
+	}
+	
+	
+	
+}
